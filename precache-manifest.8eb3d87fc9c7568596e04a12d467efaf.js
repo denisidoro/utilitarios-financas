3b24d6c9a5264e15fdf7fa7c5f378aad5d13e73c
Denis Isidoro <denis_isidoro@live.com>
2019-09-29 12:31:10 -0300

    Updates

---
+++ precache-manifest.8eb3d87fc9c7568596e04a12d467efaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0274e7d236ec5cfc70c5c5b5d951cf4",
+    "url": "/utilitarios-financas/index.html"
+  },
+  {
+    "revision": "efacfb26c025833e44dd",
+    "url": "/utilitarios-financas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "77f8c58831c8fa296d2b",
+    "url": "/utilitarios-financas/static/js/2.2c7e6e8f.chunk.js"
+  },
+  {
+    "revision": "efacfb26c025833e44dd",
+    "url": "/utilitarios-financas/static/js/main.51ab02dd.chunk.js"
+  },
+  {
+    "revision": "6db0424735d7bd61df61",
+    "url": "/utilitarios-financas/static/js/runtime-main.0fd66963.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/utilitarios-financas/static/media/logo.25bf045c.svg"
+  }
+]);
